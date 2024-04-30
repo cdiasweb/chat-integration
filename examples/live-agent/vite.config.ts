fa330c5f5c2893068f9a91495d53d236b555d779
cdiasweb <carlosdias.web@gmail.com>
2024-04-30 23:19:11 +0200

    Prod build

--- examples/live-agent/vite.config.ts
+++ examples/live-agent/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'build',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'bundle.js', // Custom bundle name
+      }
+    }
   },
   define: {
     'import.meta.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
