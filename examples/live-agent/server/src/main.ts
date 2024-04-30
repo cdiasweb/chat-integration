2f8fd9bdda83cc7b41e505f303838d4b4622aae5
cdiasweb <carlosdias.web@gmail.com>
2024-04-30 23:54:11 +0200

    Update server

--- examples/live-agent/server/src/main.ts
+++ examples/live-agent/server/src/main.ts
@@ -4,10 +4,29 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import expressWS from 'express-ws';
+import https from 'https';
+import fs from 'fs';
 
 import { gorgiasRoutes } from './gorgias/gorgias.routes';
 
-const { app } = expressWS(express());
+let server = expressWS(express());
+let httpsServer = null;
+
+if (process.env.ENVIRONMENT === 'production') {
+  const expressApp = express();
+
+  const sslOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/chatbot-farmapiel.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/chatbot-farmapiel.com/cert.pem'),
+  };
+
+  // Set up HTTPS server
+  httpsServer = https.createServer(sslOptions, expressApp);
+
+  server = expressWS(expressApp, httpsServer);
+}
+
+const { app } = server;
 
 // Increase limit to 10MB (adjust as needed)
 app.use(bodyParser.json({ limit: '10mb' }));
@@ -34,5 +53,13 @@ app.use(function (req, res, next) {
 
 gorgiasRoutes(app);
 
-app.listen(9099, '0.0.0.0');
-console.log('server is running on port 9099 for all domains');
+// Https config
+if (process.env.ENVIRONMENT === 'production' && httpsServer) {
+  // Start the HTTPS server on port 443
+  httpsServer.listen(443, () => {
+    console.log('HTTPS server with WebSockets is running on port 443');
+  });
+} else {
+  app.listen(9099, '0.0.0.0');
+  console.log('server is running on port 9099 for all domains');
+}
