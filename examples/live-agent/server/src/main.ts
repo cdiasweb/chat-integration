836c501374b773aa080545653b9726ae14e60db0
cdiasweb <carlosdias.web@gmail.com>
2024-04-30 21:52:42 +0200

    Fix server

--- examples/live-agent/server/src/main.ts
+++ examples/live-agent/server/src/main.ts
@@ -9,8 +9,25 @@ import { gorgiasRoutes } from './gorgias/gorgias.routes';
 
 const { app } = expressWS(express());
 
-app.use(cors());
-app.use(bodyParser.json());
+let origin = '*';
+
+const corsOptions = {
+  origin, // Allow only this origin
+};
+
+app.use(cors(corsOptions));
+// Increase limit to 10MB (adjust as needed)
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  next();
+});
+
 gorgiasRoutes(app);
 
 app.listen(9099, '0.0.0.0');
