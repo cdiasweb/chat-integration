83b6fb97f6b47c0739916497189e03570e609831
Carlos Dias <112416334+carlosedias@users.noreply.github.com>
2024-05-06 21:40:37 +0200

    Change colors and add extensions

--- examples/live-agent/src/config.ts
+++ examples/live-agent/src/config.ts
@@ -1,6 +1,6 @@
 import { AssistantOptions, ChatConfig } from '@voiceflow/react-chat';
-
-const imageLogo = 'https://cdiasweb.github.io/chat-integration/examples/live-agent/shared/logo.jpeg';
+import {FormExtension} from "../extensions/chatbot-extensions";
+const imageLogo = '/logo.jpeg';
 
 const IMAGE = imageLogo;
 const AVATAR = imageLogo;
@@ -10,6 +10,8 @@ export const ASSISTANT: AssistantOptions = AssistantOptions.parse({
   description: "Deja que te ayude.",
   image: IMAGE,
   avatar: AVATAR,
+  color: '#09A1B9',
+  extensions: [FormExtension]
 });
 
 export const CONFIG = ChatConfig.parse({
